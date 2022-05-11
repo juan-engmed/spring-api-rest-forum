213063dd0ecf9d163b386fbed11e30ce1e1dfb6a
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-05-11 18:52:54 -0300

    validation

--- src/main/java/br/com/juandev/forum/controller/TopicosController.java
+++ src/main/java/br/com/juandev/forum/controller/TopicosController.java
@@ -6,17 +6,22 @@ import br.com.juandev.forum.entity.Topico;
 import br.com.juandev.forum.repository.CursoRepository;
 import br.com.juandev.forum.repository.TopicoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/topicos")
+@Validated
 public class TopicosController {
 
     private final TopicoRepository repository;
@@ -32,7 +37,8 @@ public class TopicosController {
     }
 
     @PostMapping()
-    public void saveTopico(@RequestBody TopicoFormDTO topicoFromDTO){
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void saveTopico(@RequestBody @Valid TopicoFormDTO topicoFromDTO){
 
         var curso = cursoRepository.findByNome(topicoFromDTO.getNomeCurso());
 
--- src/main/java/br/com/juandev/forum/dto/TopicoFormDTO.java
+++ src/main/java/br/com/juandev/forum/dto/TopicoFormDTO.java
@@ -2,15 +2,24 @@ package br.com.juandev.forum.dto;
 
 import br.com.juandev.forum.entity.Curso;
 import br.com.juandev.forum.entity.Topico;
+import com.sun.istack.NotNull;
 import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class TopicoFormDTO {
 
+    @NotNull
+    @NotEmpty
     private String titulo;
 
+    @NotNull
+    @NotEmpty
     private String mensagem;
 
+    @NotNull
+    @NotEmpty
     private String nomeCurso;
 
     public Topico converter(TopicoFormDTO topicoFromDTO, Curso curso) {
